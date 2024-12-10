dfb0aab684d16a003519fde46b5d28f654ad1e85
prakruti-pathak <prakrutipathak109@gmail.com>
2024-12-10 11:21:08 +0530

    Singleton design pattern example

---
+++ SingletonDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SingletonDemo;
+
+Singleton singleton =  Singleton.GetInstance;
+singleton.PrintDetails("heloo");
+
+
+Singleton singleton1 = Singleton.GetInstance;
+singleton1.PrintDetails("heloo1");
+Console.ReadLine();
+
+
+
+
+
---
+++ SingletonDemo/Singleton.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//create multiple instance every time
+//so make class as sealed and constructor as private (singleton pattern)
+
+//without sealed also we can't inherited class bcoz thereis private constructor
+//but we can inherited it in that class(nested class) so another instance is also created 
+//so that why we are using sealed keyword
+
+namespace SingletonDemo
+{
+    //Sealed restricts the inheritance
+    public sealed class Singleton
+    {
+        private static int counter =0;
+        private static Singleton instance=null;
+       //private constructor ensure that object is not instantiated other than with in the class itself
+        private Singleton() 
+        {
+            counter++;
+            Console.WriteLine("Counter Value "+counter.ToString());
+        }
+        //public property is used to return only one instance of the class leveraging
+        //on the private property
+        public static Singleton GetInstance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Singleton();
+                return instance;
+            }
+        }
+        //public method which can be invoked through the singleton instance
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
